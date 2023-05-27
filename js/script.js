985d9873392916f9060a4d1c77be89475621fa80
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2023-05-27 11:04:46 -0300

    Finalizando o Box Destinos e mudando logo da barra de navegação

--- js/script.js
+++ js/script.js
@@ -4,4 +4,12 @@ $(function () {
     e.preventDefault();
     $(".nav").toggleClass("active");
   });
+
+  $(window).scroll(function () {
+    if($(this).scrollTop() > 100) {
+      $('.header').addClass('fixed');
+    } else {
+      $('.header').removeClass('fixed');
+    }
+  });
 });
